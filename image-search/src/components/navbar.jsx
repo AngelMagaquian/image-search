5f557cebbe7da646b7441a9eb5928e6ac108f680
Angel Magaquian <angel.magaquian@incluit.com>
2022-03-05 01:24:54 -0300

    search component

--- image-search/src/components/navbar.jsx
+++ image-search/src/components/navbar.jsx
@@ -1,13 +1,10 @@
-const navbar = ({handleSearch}) => {
+const navbar = () => {
   
   return (
         <nav className="navbar fixed-top navbar-dark bg-dark">
             <div className="container-fluid">
                 <a className="navbar-brand">Image Search!</a>
-                <form className="d-flex">
-                    <input className="form-control me-2" type="search" id='search' placeholder="Search" aria-label="Search" autoComplete="off"/>
-                    <button className="btn btn-primary" type="button" onClick={() => handleSearch(document.getElementById('search').value)}>Search</button>
-                </form>
+                
             </div>
         </nav>
   )
