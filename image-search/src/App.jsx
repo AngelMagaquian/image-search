94924a0acfad66b3ca5c4a7da356f2f5f65b0f4a
Angel Magaquian <angelmagakhian@gmail.com>
2022-03-07 00:59:12 -0300

    updates

--- image-search/src/App.jsx
+++ image-search/src/App.jsx
@@ -13,6 +13,7 @@ import { unsplash } from './access/unsplash'
 function App() {
   //state
   const [image, setImage]= useState([])
+
   //llamada a la API
   const handleSearch= async(value) =>{
     const res = await fetch(unsplash.endPoint_Search+value,{headers:{Authorization:'Client-ID '+unsplash.clientID}})
@@ -21,10 +22,18 @@ function App() {
     setImage(data.results)
   }
 
-
+  const handleRandom= async()=>{
+   /*  const res = await fetch(unsplash.endPoint_Random,{headers:{Authorization:'Client-ID '+unsplash.clientID}}) */
+    const res = await fetch(unsplash.endPoint_Search+'developers',{headers:{Authorization:'Client-ID '+unsplash.clientID}})
+    const data = await res.json()
+    console.log(data)
+    setImage(data.results)
+    console.log(image)
+  }
+ 
   
-
-   
+ 
+  
 
   const handleLike = (id) =>{
     let img = image.find((image=> id == image.id))
@@ -36,6 +45,8 @@ function App() {
     }
     setImage([...image], [img.liked_by_user = !liked])    
   }
+
+  
 
   return (
     <div className="app-container">
@@ -55,7 +66,7 @@ function App() {
        
        <div className="container image-container">
          <div className="row ">
-          {image.length ? 
+          {image.length > 0 ? 
               image.map(img =>(<ImageCards key={img.id} data={img} handleLike={handleLike}/>))
               : null
           }
--- image-search/src/access/unsplash.js
+++ image-search/src/access/unsplash.js
@@ -3,7 +3,7 @@ const endPoint = ''; */
 const unsplash ={
     clientID: 'T5e6VHuzUIB-jSv-9JnIg56_efljJe9E3R0amCA4xOc',
     endPoint_Search: 'https://api.unsplash.com/search/photos?per_page=20&order_by=popular&query=',
-    endPoint_Random: 'https://api.unsplash.com/photos/random?per_page=20&',
+    endPoint_Random: 'https://api.unsplash.com/photos/random?per_page=20',
     
 
 }
