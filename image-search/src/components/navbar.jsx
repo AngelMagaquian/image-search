fd7a6569993813a565163356074644ef6073bbb2
Angel Magaquian <angelmagakhian@gmail.com>
2022-03-08 01:22:04 -0300

    complete app v1

--- image-search/src/components/navbar.jsx
+++ image-search/src/components/navbar.jsx
@@ -1,9 +1,13 @@
+import Logo from '../assets/LogoIS.png'
 const navbar = () => {
   
   return (
-        <nav className="navbar fixed-top navbar-dark bg-dark">
+        <nav className="navbar fixed-top navbar-dark">
             <div className="container-fluid">
-                <a className="navbar-brand">Image Search!</a>
+            <a class="navbar-brand" href="#">
+              <img src={Logo} alt="Image Search!" width="30" height="30"/>
+              
+            </a>
                 
             </div>
         </nav>
